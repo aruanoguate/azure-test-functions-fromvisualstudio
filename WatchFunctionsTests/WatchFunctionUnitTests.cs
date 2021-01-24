741de68c5437a2dbf4613bc5d9a19725e53a8ae1
Alvaro Enrique Ruano <alvaro.ruano@compiqsolutions.com>
2021-01-24 12:22:33 -0600

    Adding TestWatchFunctionSuccess method

--- WatchFunctionsTests/WatchFunctionUnitTests.cs
+++ WatchFunctionsTests/WatchFunctionUnitTests.cs
@@ -1,14 +1,47 @@
 using System;
 using Xunit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace WatchFunctionsTests
 {
     public class WatchFunctionUnitTests
     {
         [Fact]
-        public void Test1()
+        public void TestWatchFunctionSuccess()
         {
+            // This statement creates a mock HTTP context and an HTTP request. 
+            // The request includes a query string that includes the model parameter, which is set to abc.
+            var queryStringValue = "abc";
+            var request = new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Query = new QueryCollection
+                (
+                    new System.Collections.Generic.Dictionary<string, StringValues>()
+                    {
+                        { "model", queryStringValue }
+                    }
+                )
+            };
 
+            // This statement creates a dummy logger.
+            var logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+
+            // These statements invoke the WatchInfo function, passing in the dummy request and logger as parameters.
+            var response = WatchPortalFunction.WatchInfo.Run(request, logger);
+            response.Wait();
+
+            // Check that the response is an "OK" response
+            Assert.IsAssignableFrom<OkObjectResult>(response.Result);
+
+            // Check that the contents of the response are the expected contents
+            var result = (OkObjectResult)response.Result;
+            dynamic watchinfo = new { Manufacturer = "abc", CaseType = "Solid", Bezel = "Titanium", Dial = "Roman", CaseFinish = "Silver", Jewels = 15 };
+            string watchInfo = $"Watch Details: {watchinfo.Manufacturer}, {watchinfo.CaseType}, {watchinfo.Bezel}, {watchinfo.Dial}, {watchinfo.CaseFinish}, {watchinfo.Jewels}";
+            Assert.Equal(watchInfo, result.Value);
         }
     }
 }
