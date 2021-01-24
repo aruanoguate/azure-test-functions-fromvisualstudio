31d4cf2249c63f7687f049b8732be29a76c2f455
Alvaro Enrique Ruano <alvaro.ruano@compiqsolutions.com>
2021-01-24 12:24:42 -0600

    Adding TestWatchFunctionFailureNoModel & TestWatchFunctionFailureNoQueryString

--- WatchFunctionsTests/WatchFunctionUnitTests.cs
+++ WatchFunctionsTests/WatchFunctionUnitTests.cs
@@ -43,5 +43,50 @@ namespace WatchFunctionsTests
             string watchInfo = $"Watch Details: {watchinfo.Manufacturer}, {watchinfo.CaseType}, {watchinfo.Bezel}, {watchinfo.Dial}, {watchinfo.CaseFinish}, {watchinfo.Jewels}";
             Assert.Equal(watchInfo, result.Value);
         }
+
+        [Fact]
+        public void TestWatchFunctionFailureNoQueryString()
+        {
+            var request = new DefaultHttpRequest(new DefaultHttpContext());
+            var logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+
+            var response = WatchPortalFunction.WatchInfo.Run(request, logger);
+            response.Wait();
+
+            // Check that the response is an "Bad" response
+            Assert.IsAssignableFrom<BadRequestObjectResult>(response.Result);
+
+            // Check that the contents of the response are the expected contents
+            var result = (BadRequestObjectResult)response.Result;
+            Assert.Equal("Please provide a watch model in the query string", result.Value);
+        }
+
+        [Fact]
+        public void TestWatchFunctionFailureNoModel()
+        {
+            var queryStringValue = "abc";
+            var request = new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Query = new QueryCollection
+                (
+                    new System.Collections.Generic.Dictionary<string, StringValues>()
+                    {
+                        { "not-model", queryStringValue }
+                    }
+                )
+            };
+
+            var logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+
+            var response = WatchPortalFunction.WatchInfo.Run(request, logger);
+            response.Wait();
+
+            // Check that the response is an "Bad" response
+            Assert.IsAssignableFrom<BadRequestObjectResult>(response.Result);
+
+            // Check that the contents of the response are the expected contents
+            var result = (BadRequestObjectResult)response.Result;
+            Assert.Equal("Please provide a watch model in the query string", result.Value);
+        }
     }
 }
